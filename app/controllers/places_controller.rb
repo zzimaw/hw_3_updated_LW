1b13b44b5a106b7003127c35d660e43632443999
Linda Wang <linda.wang@kellogg.northwestern.edu>
2024-02-21 03:52:10 +0000

    places input

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,33 @@
 class PlacesController < ApplicationController
   def index
-    render :template => "places/index"
+    # find all Company rows
+    @places = Place.all
+    # render companies/index view
+  end
+  
+  def show
+    # find a Place
+    @places = Place.find_by({ "id" => params["id"] })
+
   end
 
-  def show
-
-  end 
-
   def new
-  end 
+    # render view with new Company form
+  end
+
+  def create
+    # start with a new Company
+    @place = Place.new
+
+    # assign user-entered form data to Company's columns
+    @place["name"] = params["name"]
+
+    # save Company row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
 
 
 end
